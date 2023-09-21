eca887772ac48ac60bdf1a5401aa081b6a55c8d0
LinaRomilson <lina@romilson.se>
2023-09-21 20:01:19 +0200

    buggar fixade

--- TjugoEtt.py
+++ TjugoEtt.py
@@ -1,6 +1,7 @@
 from Kortlek import Kortleken
 from Spelare import Spelarna
 import os
+import time
 
 
 def main():
@@ -28,27 +29,38 @@ def main():
 
         # Visa spelarnas händer, endast datorns första kort
         print(f"{spelare_namn}s hand: {', '.join(spelare.hand)} ({spelare.räkna_poäng()} poäng)")
+        time.sleep(1)       # Väntar 1 sekund innan det går vidare
         print(f"Datorns hand: {', '.join(dator.hand[:1])} och {len(dator.hand) - 1} fler")
-
+        time.sleep(2)       # Väntar 2 sekunder innan det går vidare
         # Kolla om någon har vunnit
-        avsluta_spelet(spelare_namn, spelare.räkna_poäng(), dator.räkna_poäng())
+        avslutat = avsluta_spelet(spelare_namn, spelare.räkna_poäng(), dator.räkna_poäng())
+        # Om vinnaren är hittad ska spelet avslutas
+        if avslutat:
+            break
 
-        spela_igen = input("Vill du dra ett kort till?")
+        spela_igen = input("Vill du dra ett kort till? (ja/nej) ")
         if spela_igen.lower() != "ja":
             # Om datorn har mindre än 17 poäng, fortsätt dra kort
             while dator.räkna_poäng() < 17:
                 dator.dra_kort(kortlek)
+                time.sleep(2)   # Väntar 2 sekunder innan det går vidare
                 print(f"Datorns hand: {', '.join(dator.hand)} ({dator.räkna_poäng()} poäng)")
 
-            # Visa spelarnas händer, endast datorns första kort
-            print(f"{spelare_namn}s hand: {', '.join(spelare.hand)} ({spelare.räkna_poäng()} poäng)")
-            print(f"Datorns hand: {', '.join(dator.hand)} ({dator.räkna_poäng()} poäng)")
-
             # Kolla om någon har vunnit efter att datorn har dragit sina kort
-            avsluta_spelet(spelare_namn, spelare.räkna_poäng(), dator.räkna_poäng())
+            avslutat = avsluta_spelet(spelare_namn, spelare.räkna_poäng(), dator.räkna_poäng())
+            # Avsluta om vinnaren är hittad
+            if avslutat:
+                break
 
-            # Avluta loopen om spelaren inte vill dra fler kort
-            break
+            if spelare.räkna_poäng() < dator.räkna_poäng():
+                print(f"Datorn vann! (Datorns poäng: {dator.räkna_poäng()} {spelare_namn}s poäng: {spelare.räkna_poäng()})")
+                break
+            elif dator.räkna_poäng() > spelare.räkna_poäng():
+                print(f"{spelare_namn} vann! (Datorns poäng: {dator.räkna_poäng()} {spelare_namn}s poäng: {spelare.räkna_poäng()})")
+                break
+
+            # Avsluta loopen om spelaren inte vill dra fler kort
+            #break
         else:
             print("-" * 10 + "Nästa omgång" + "-" * 10)
 
@@ -57,16 +69,23 @@ def main():
 def avsluta_spelet(spelare_namn, spelare_poäng, dator_poäng):
     if spelare_poäng == 21 and dator_poäng == 21:
         print("Datorn vann! Nästa gång tar du det", spelare_namn)
+        return True
     elif spelare_poäng == 21:
         print(f"{spelare_namn} vinner med exakt 21 poäng!")
+        return True
     elif dator_poäng == 21:
         print("Datorn vinner med exakt 21 poäng!")
+        return True
     elif spelare_poäng > 21 and dator_poäng > 21:
         print("Det blev lika (båda har över 21)!")
+        return True
     elif spelare_poäng > 21:
         print(f"Datorn vann! ({spelare_namn} har över 21)")
+        return True
     elif dator_poäng > 21:
         print(f"{spelare_namn} vinner! (Datorn har över 21)")
+        return True
+    return False
 
 
 if __name__ == "__main__":
